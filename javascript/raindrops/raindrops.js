c8c1db3b59c199c8d9bd30c1cc5c6c0007b7e91c
Kevin Lai <kevin.km.lai@gmail.com>
2020-03-01 22:40:31 +1100

    [Raindrops] Major refactoring

--- javascript/raindrops/raindrops.js
+++ javascript/raindrops/raindrops.js
@@ -1,25 +1,7 @@
 export const convert = (num) => {
-  if ( num % 3 === 0 && num % 5 === 0 && num % 7 === 0){
-    return 'PlingPlangPlong'
-  }
-  if ( num % 3 === 0 && num % 5 === 0){
-    return 'PlingPlang'
-  }
-  if ( num % 3 === 0 && num % 7 === 0){
-    return 'PlingPlong'
-  }
-  if ( num % 5 === 0 && num % 7 === 0){
-    return 'PlangPlong'
-  }
-
-  if ( num % 3 === 0 ){
-    return 'Pling'
-  }
-  if ( num % 5 === 0 ){
-    return 'Plang'
-  }
-  if ( num % 7 === 0 ){
-    return 'Plong'
-  }
-  return num.toString()
+  let result = ''
+  if (num % 3 === 0) result+= "Pling"
+  if (num % 5 === 0) result += "Plang"
+  if (num % 7 === 0) result += "Plong"
+  return result || num.toString()
 };
